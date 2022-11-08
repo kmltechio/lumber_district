e6bb2f76c01daad3eee1216a25b2b18b0f41202c
Kevin Lilly <100501026+kmltechio@users.noreply.github.com>
2022-11-08 13:17:02 -0700

    Stub 2nd engine

--- myAutomation.h
+++ myAutomation.h
@@ -1,2 +1,8 @@
-// locomotive roster first
+//
+:/ locomotive roster first
+//
+// BLI w/ Paragon2 sound
 ROSTER(9916,"CBQ E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Indistry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
+// Kato w/ Digitrax XXX decoder
+// XXX verify number, decoder and functions
+ROSTER(3, "CBQ NW2","xxx")
