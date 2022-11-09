16f31347ddd5d87ab86479781ef20fd50283fd28
Kevin Lilly <kevin@kmltech.io>
2022-11-08 17:57:06 -0700

    fixed bad comment

--- myAutomation.h
+++ myAutomation.h
@@ -1,6 +1,6 @@
-//
-:/ locomotive roster first
-//
+/*
+ * Locomotive Roster
+ */
 // BLI w/ Paragon2 sound
 ROSTER(9916,"CBQ E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Indistry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
 // Kato w/ Digitrax XXX decoder
