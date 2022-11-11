873da39b7148a2efda2efb08405cdddb981f2d61
Kevin Lilly <kevin@kmltech.io>
2022-11-11 08:35:27 -0700

    cleaned up roster

--- myAutomation.h
+++ myAutomation.h
@@ -2,7 +2,6 @@
  * Locomotive Roster
  */
 // BLI w/ Paragon2 sound
-ROSTER(9916,"CBQ E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Indistry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
-// Kato w/ Digitrax XXX decoder
-// XXX verify number, decoder and functions
-ROSTER(3, "CBQ NW2","xxx")
+ROSTER(9916,"CB&Q E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Indistry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
+// Kato w/ Digitrax DN123K3 decoder
+ROSTER(9211,"CB&Q NW2","Lights")
