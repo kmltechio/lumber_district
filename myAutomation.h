4660d7f41bdf62672287032b141b2cb106ddcae2
Kevin Lilly <kevin@kmltech.io>
2024-07-12 19:32:32 -0600

    autostart settings for power zones

--- myAutomation.h
+++ myAutomation.h
@@ -1,7 +1,14 @@
-/*
- * Locomotive Roster
- */
+// Locomotive Roster
 // BLI w/ Paragon2 sound
 ROSTER(9916,"CB&Q E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Indistry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
 // Kato w/ Digitrax DN123K3 decoder
 ROSTER(9211,"CB&Q NW2","Lights")
+// Non-DDC engines on the B track
+ROSTER(666,"CD Engine")
+
+// Track Manager
+AUTOSTART
+SET_TRACK(A, MAIN)
+SETLOCO(666) SET_TRACK(B, DCX) // set the DC locos to Track B aka trolley and reverse polarity so that Engine Driver looks correct
+POWERON
+DONE
