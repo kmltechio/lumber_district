14fea0ee96b22adfba98eee3f46d0345effe92b7
Kevin Lilly <kevin@kmltech.io>
2025-05-06 07:49:03 -0600

    pins and touch ctl loop

--- myAutomation.h
+++ myAutomation.h
@@ -1,5 +1,50 @@
 #include "myRoster.h"
-#include "myHal.h"
+
+// Set up our I2C expanders
+HAL(EXIOExpander, 800, 18, 0x65) // touch panel nano
+// TODO HAL(EXIOExpander, 850, 18, 0x66) // lower h-bridge nano
+// TODO HAL(EXIOExpander, 900, 18, 0x67) // upper h-bridge nano
+// TODO HAL(DFPlayer, 10000, 10, Serial1)
+
+// Alias all Mega & IOExpander pins here
+// see https://dcc-ex.com/reference/hardware/pin-vpin-allocations.html
+// Mega Pin Assignements
+ALIAS(PIN_FAN, 2)
+// reserve : 22 possible use for I2C interrupts
+ALIAS(SENS_IR_TROLLEY_WEST, 23)
+ALIAS(SENS_IR_TROLLEY_MID, 24)
+ALIAS(SENS_IR_TROLLEY_EAST, 25)
+ALIAS(SENS_IR_TUNNEL_WEST, 26)
+ALIAS(SENS_IR_TUNNEL_EAST, 27)
+// ALIAS(SENS_IR_STAGING_WB, 28)
+// ALIAS(SENS_IR_STAGING_EB, 29)
+// usable : D30-D49
+// usable : A8-A15
+
+// Touch Panel Nano Pin Assignments
+// unusable : D0 TX
+// unusable : D1 RX
+ALIAS(TOUCH_2_1, 800) // D2
+ALIAS(TOUCH_2_2, 801) // D3
+ALIAS(TOUCH_2_3, 802) // D4
+ALIAS(TOUCH_2_4, 803) // D5
+ALIAS(TOUCH_2_5, 804) // D6
+ALIAS(TOUCH_2_6, 805) // D7
+ALIAS(TOUCH_2_7, 806) // D8
+ALIAS(TOUCH_2_8, 807) // D9
+ALIAS(TOUCH_2_9, 808) // D10
+// ALIAS(usable, 809) // D11
+ALIAS(TOUCH_1_1, 810) // D12
+// ALIAS(usable, 811) // D13 may be tied to the onboard LED
+ALIAS(TOUCH_1_2, 812) // A0
+ALIAS(TOUCH_1_3, 813) // A1
+ALIAS(TOUCH_1_4, 814) // A2
+ALIAS(TOUCH_1_5, 815) // A3
+// unusable : A4 SDA
+// unusable : A5 SDA
+ALIAS(TOUCH_1_6, 816) // A6
+ALIAS(SENS_HEAT, 817) // A7
+
 
 // Startup our track power
 AUTOSTART                                     // This sequence auto runs at bootup
@@ -9,8 +54,8 @@ POWERON                                       // Turn on power to both tracks
 DONE
 
 // Autostart our fan controller
-#define HEAT_ON_VAL  150 // ~95degF based on empirical testing
-#define HEAT_OFF_VAL 142 // about room temp
+#define HEAT_ON_VAL  165 // ~95degF based on empirical testing
+#define HEAT_OFF_VAL 155 // about room temp
 ALIAS(SEQ_FAN_CTL)
 SEQUENCE(SEQ_FAN_CTL)
   AUTOSTART
@@ -20,7 +65,31 @@ SEQUENCE(SEQ_FAN_CTL)
   RESET(PIN_FAN)
   FOLLOW(SEQ_FAN_CTL)
 
-// TODO create a macro for turnout/touch/h-bridge control
+
+#define TOUCH_READ_MS 1
+#define TOUCH_SHOW_MS 200
+#define TOUCH_CTL(seq, sens, latc) \
+ALIAS(seq) \
+ALIAS(latc, sens - 700) \
+SEQUENCE(seq) \
+  AUTOSTART \
+  IF(sens) ENDIF \
+  DELAYMS(TOUCH_READ_MS) \
+  IF(sens) \
+    IFNOT(latc) LATCH(latc) ENDIF \
+  ELSE \
+    IF(latc) UNLATCH(latc) ENDIF \
+  ENDIF \
+  IF(latc) SET(sens) \
+  ELSE RESET(sens) \
+  ENDIF \
+  DELAYMS(TOUCH_SHOW_MS) \
+  FOLLOW(seq)
+
+TOUCH_CTL(SEQ_1_1, TOUCH_1_1, STATE_1_1)
+
+// TODO create a macro for turnout/h-bridge control using STATE_1_1
+
 
 // Include all remaining routes and automations below
 #include "myTrolley.h"
