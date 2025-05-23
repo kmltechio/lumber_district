0cddb4af4b8fc91591de0f341a8d1631a7642be1
Kevin Lilly <kevin@kmltech.io>
2025-05-23 16:32:59 -0600

    clean up pin assignments and implement turnout macro

--- Makefile
+++ Makefile
@@ -12,8 +12,9 @@ compile: compile_cs_ex compile_io_ex
 
 CS_EX := CommandStation-EX
 CS_EX_BRANCH := master
-CS_EX_CONFIGS := $(CS_EX)/config.h $(CS_EX)/myAutomation.h $(CS_EX)/myRoster.h \
-	$(CS_EX)/myTrolley.h $(CS_EX)/myPassengerLoop.h
+CS_EX_CONFIGS := $(CS_EX)/config.h $(CS_EX)/myAutomation.h \
+	$(CS_EX)/myPassengerLoop.h $(CS_EX)/myPins.h $(CS_EX)/myRoster.h \
+	$(CS_EX)/myTrolley.h
 CS_EX_BOARD := arduino:avr:mega
 
 $(CS_EX):
--- myAutomation.h
+++ myAutomation.h
@@ -1,57 +1,12 @@
 #include "myRoster.h"
+#include "myPins.h"
 
-// Set up our I2C expanders
-HAL(EXIOExpander, 800, 18, 0x65) // touch panel nano
-// TODO HAL(EXIOExpander, 850, 18, 0x66) // lower h-bridge nano
-// TODO HAL(EXIOExpander, 900, 18, 0x67) // upper h-bridge nano
-// TODO HAL(DFPlayer, 10000, 10, Serial1)
-
-// Alias all Mega & IOExpander pins here
-// see https://dcc-ex.com/reference/hardware/pin-vpin-allocations.html
-// Mega Pin Assignements
-ALIAS(PIN_FAN, 2)
-// reserve : 22 possible use for I2C interrupts
-ALIAS(SENS_IR_TROLLEY_WEST, 23)
-ALIAS(SENS_IR_TROLLEY_MID, 24)
-ALIAS(SENS_IR_TROLLEY_EAST, 25)
-ALIAS(SENS_IR_TUNNEL_WEST, 26)
-ALIAS(SENS_IR_TUNNEL_EAST, 27)
-// ALIAS(SENS_IR_STAGING_WB, 28)
-// ALIAS(SENS_IR_STAGING_EB, 29)
-// usable : D30-D49
-// usable : A8-A15
-
-// Touch Panel Nano Pin Assignments
-// unusable : D0 TX
-// unusable : D1 RX
-ALIAS(TOUCH_2_1, 800) // D2
-ALIAS(TOUCH_2_2, 801) // D3
-ALIAS(TOUCH_2_3, 802) // D4
-ALIAS(TOUCH_2_4, 803) // D5
-ALIAS(TOUCH_2_5, 804) // D6
-ALIAS(TOUCH_2_6, 805) // D7
-ALIAS(TOUCH_2_7, 806) // D8
-ALIAS(TOUCH_2_8, 807) // D9
-ALIAS(TOUCH_2_9, 808) // D10
-// ALIAS(usable, 809) // D11
-ALIAS(TOUCH_1_1, 810) // D12
-// ALIAS(usable, 811) // D13 may be tied to the onboard LED
-ALIAS(TOUCH_1_2, 812) // A0
-ALIAS(TOUCH_1_3, 813) // A1
-ALIAS(TOUCH_1_4, 814) // A2
-ALIAS(TOUCH_1_5, 815) // A3
-// unusable : A4 SDA
-// unusable : A5 SDA
-ALIAS(TOUCH_1_6, 816) // A6
-ALIAS(SENS_HEAT, 817) // A7
-
-
-// Startup our track power
+// Setup our track power but don't start it...do that from the controller app
 AUTOSTART                                     // This sequence auto runs at bootup
 SET_TRACK(A, MAIN)                            // Track A is set for DCC
 SETLOCO(TROLLEY_LOCO_NUM) SET_TRACK(B, DC)    // Track B set for DC, specifically the trolley
-POWERON                                       // Turn on power to both tracks
 DONE
+
 
 // Autostart our fan controller
 #define HEAT_ON_VAL  165 // ~95degF based on empirical testing
@@ -66,29 +21,44 @@ SEQUENCE(SEQ_FAN_CTL)
   FOLLOW(SEQ_FAN_CTL)
 
 
-#define TOUCH_READ_MS 1
-#define TOUCH_SHOW_MS 200
-#define TOUCH_CTL(seq, sens, latc) \
-ALIAS(seq) \
-ALIAS(latc, sens - 700) \
-SEQUENCE(seq) \
+/* This macro does the following
+ * 1. Create a handy alias for the turnout
+ * 2. Create a turnout object but with a bogus pin since we need 2 pins per kato
+ *    turnout with special pulsing logic
+ * 3. ONCLOUSE/ONTHROW event handlers to do the pulsing logic
+ * 4. A sequence (that follows itself) to listing for touch toggle changes and
+ *    throw/close the turnout accordingly
+*/
+#define KATO_PULSE_MS 20
+#define KATO_TURNOUT(ali, turn, sens, hba, hbb, desc) \
+ALIAS(ali, turn) \
+PIN_TURNOUT(turn, 0, desc) \
+ONCLOSE(turn) SET(hba) DELAY(KATO_PULSE_MS) RESET(hba) DONE \
+ONTHROW(turn) SET(hbb) DELAY(KATO_PULSE_MS) RESET(hbb) DONE \
+SEQUENCE(turn+2000) \
   AUTOSTART \
-  IF(sens) ENDIF \
-  DELAY(TOUCH_READ_MS) \
   IF(sens) \
-    IFNOT(latc) LATCH(latc) ENDIF \
+    AT(-sens) CLOSE(turn) FOLLOW(turn+2000) \
   ELSE \
-    IF(latc) UNLATCH(latc) ENDIF \
+    AT(sens) THROW(turn) FOLLOW(turn+2000) \
   ENDIF \
-  IF(latc) SET(sens) \
-  ELSE RESET(sens) \
-  ENDIF \
-  DELAY(TOUCH_SHOW_MS) \
-  FOLLOW(seq)
+DONE
 
-TOUCH_CTL(SEQ_1_1, TOUCH_1_1, STATE_1_1)
-
-// TODO create a macro for turnout/h-bridge control using STATE_1_1
+KATO_TURNOUT(TRN_1_1,  1, TOU_1_1, HBL_1_1_A, HBL_1_1_B, "E. loop (fake X)")
+KATO_TURNOUT(TRN_1_2,  2, TOU_1_2, HBL_1_2_A, HBL_1_2_B, "Bakery")
+KATO_TURNOUT(TRN_1_3,  3, TOU_1_3, HBL_1_3_A, HBL_1_3_B, "Interchange")
+KATO_TURNOUT(TRN_1_4,  4, TOU_1_4, HBL_1_4_A, HBL_1_4_B, "Scrap yard")
+KATO_TURNOUT(TRN_1_5,  5, TOU_1_5, HBL_1_5_A, HBL_1_5_B, "Runaround (fake X)")
+KATO_TURNOUT(TRN_1_6,  6, TOU_1_6, HBL_1_6_A, HBL_1_6_B, "Brewery / Lumber yard")
+KATO_TURNOUT(TRN_2_1,  7, TOU_2_1, HBU_2_1_A, HBU_2_1_B, "todo")
+KATO_TURNOUT(TRN_2_2,  8, TOU_2_2, HBU_2_2_A, HBU_2_2_B, "todo")
+KATO_TURNOUT(TRN_2_3,  9, TOU_2_3, HBU_2_3_A, HBU_2_3_B, "todo")
+KATO_TURNOUT(TRN_2_4, 10, TOU_2_4, HBU_2_4_A, HBU_2_4_B, "todo")
+KATO_TURNOUT(TRN_2_5, 11, TOU_2_5, HBU_2_5_A, HBU_2_5_B, "todo")
+KATO_TURNOUT(TRN_2_6, 12, TOU_2_6, HBU_2_6_A, HBU_2_6_B, "todo")
+KATO_TURNOUT(TRN_2_7, 13, TOU_2_7, HBU_2_7_A, HBU_2_7_B, "todo")
+KATO_TURNOUT(TRN_2_8, 14, TOU_2_8, HBU_2_8_A, HBU_2_8_B, "todo")
+KATO_TURNOUT(TRN_2_9, 15, TOU_2_9, HBU_2_9_A, HBU_2_9_B, "todo")
 
 
 // Include all remaining routes and automations below
