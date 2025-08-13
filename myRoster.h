d0fc4f19424fb9214f14d7afe90a0becd1fed7a9
Kevin Lilly <kevin@kmltech.io>
2025-08-13 10:26:04 -0600

    added full roster of all engines including dc

--- myRoster.h
+++ myRoster.h
@@ -1,15 +1,24 @@
-// BLI w/ Paragon2 DCC & Sound
-ROSTER(9916,"CB&Q E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Industry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
-
 // BLI w/ Paragon4 DCC & Sound
 ROSTER(169,"CB&Q F7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Industry/Lumber/2ndHorn/Tracks/CabLight/CompHorn/PlayMacro/RecMacro/Squeal")
 
-// Kato w/ Digitrax DN123K3 decoder
-ROSTER(9211,"CB&Q NW2","Lights")
+// Atlas DC
+ROSTER(208,"CB&Q GP-7")
+
+// Atlas DC
+ROSTER(329,"CB&Q SD-9")
 
 // Trolley (realistically can be any DC engine that runs on Track B)
 #define TROLLEY_LOCO_NUM 666
 ROSTER(TROLLEY_LOCO_NUM,"DC Trolley")
 
-// Any single DC engine as long as mainline power is set to DC
-ROSTER(TROLLEY_LOCO_NUM+1,"DC Mainline")
+// Kato w/ Digitrax DN123K3 decoder
+ROSTER(9211,"CB&Q NW2","Lights")
+
+//Con-Cor DC
+ROSTER(9280,"CB&Q SW1200")
+
+// Kato DC
+ROSTER(9909,"CB&Q E5A")
+
+// BLI w/ Paragon2 DCC & Sound
+ROSTER(9916,"CB&Q E7A","Lights/*Bell/*Horn/Couple/DynBrake/EngUp/EngDown/MARS/Vol/Startup/Shutdown/Fan/Air/Brake/XHorn/Passenger/Freight/Maint/Radio/City/Farm/Industry/Lumber/2ndHorn//CabLight/Dimmer/Play/Record/Squeal")
